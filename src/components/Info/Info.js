d18bfbb16e9783e89cc8aa4a7836410439602478
andrbutler <andrbutler@yahoo.com>
2018-01-23 16:43:14 -0500

    fix spelling errors, increase height of close button in detail view for mobile devices.

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -8,7 +8,7 @@ const info = () => {
                 <div className='Bio'>
                     <h1> Hello! My name is Andrew Butler!</h1>
                     <h2> I am a self taught developer, with a current focus on front-end
-                        web-development. I am always pursuing new chalenges and learning 
+                        web-development. I am always pursuing new challenges and learning 
                         new skills. Hoping to work with people who give me the opportunity
                         to solve problems and irrevocably change the world.
                         When I'm not coding I enjoy hiking, running, 
@@ -18,7 +18,7 @@ const info = () => {
                 <div className='Portrait' />
             </div>
             <div className='Skills'>
-                    <ul className='Skill'><h1>Skills I'm currently profecient in:</h1>
+                    <ul className='Skill'><h1>Skills I'm currently proficient in:</h1>
                     <li>html</li>
                     <li>css</li>
                     <li>React</li>
